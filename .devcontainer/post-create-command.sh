3f535dd34341264734f6caf4e1f44cbc1d9437a0
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-12-24 08:29:26 +0000

    devset

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo chown -R $USERNAME:$USERNAME /workspaces/wave-function-collapse
-unset $USERNAME
+USERNAME=$USERNAME
+SSH="/home/$USERNAME/.ssh/"
+IDRSA="$SSH/id_rsa" 
+
+sudo chown -R "$USERNAME:$USERNAME" "$SSH"
+sudo chmod 700 "$SSH"
+sudo chmod 600 "$IDRSA"
+
+eval "$(ssh-agent -s)"
+ssh-add "$IDRSA"
