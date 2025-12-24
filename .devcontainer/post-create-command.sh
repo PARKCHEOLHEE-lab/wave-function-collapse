c09b37abd79fd99c1a442a260ec427ac40a1df93
PARKCHEOLHEE-lab <qhqhahah@naver.com>
2025-12-24 16:07:44 +0900

    devset

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo chown -R $USERNAME:$USERNAME /workspaces/wave-function-collapse
+unset $USERNAME
--- post-create-command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pre-commit install
-
-chmod 600 /root/.ssh/id_rsa
-
-ssh-agent bash -c 'ssh-add /root/.ssh/id_rsa'
-
